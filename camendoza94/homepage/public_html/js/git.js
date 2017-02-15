cac17da909d51084986e89d4175615b68fb5afbc
camendoza94 <ca.mendoza968@uniandes.edu.co>
2017-02-15 06:42:19 -0500

    Added source. Changed info and some CSS styling. Enabled JQuery function

--- camendoza94/homepage/public_html/js/git.js
+++ camendoza94/homepage/public_html/js/git.js
@@ -1,3 +1,5 @@
+//Original from http://yonaba.github.io/2012/08/14/List-your-GitHub-projects-using-JavaScript-and-jQuery.md.html
+
 jQuery.githubUser = function(username, callback) {
    jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback);
 };
@@ -15,6 +17,9 @@ jQuery.fn.loadRepositories = function(username) {
         $(repos).each(function() {
             if (this.name !== (username.toLowerCase()+'.github.com')) {
                 list.append('<dt><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a> <em>'+(this.language?('('+this.language+')'):'')+'</em></dt>');
+                if (this.description === null){
+                    this.description = "There's no description for this repo";
+                }
                 list.append('<dd>' + this.description +'</dd>');
             }
         });      
